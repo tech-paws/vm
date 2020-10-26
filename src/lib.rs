3cc2b7f310ebb4e83ea78a1086f0acccaa84302d
Andrey Kabylin <andrey@kabylin.ru>
2020-10-26 17:06:15 +0700

    RegionAllocator

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #[repr(C)]
 pub struct RegionMemoryBuffer {
     pub size: u64,
-    pub base: *const u8,
+    pub base: *mut u8,
     pub offset: usize,
 }
 
@@ -10,11 +10,85 @@ extern "C" {
 
     pub fn create_region_memory_buffer(size: u64) -> RegionMemoryBuffer;
 
-    pub fn region_memory_buffer_emplace_region(buffer: *mut RegionMemoryBuffer, size: u64) -> RegionMemoryBuffer;
+    pub fn region_memory_buffer_emplace_region(
+        buffer: *mut RegionMemoryBuffer,
+        size: u64,
+    ) -> RegionMemoryBuffer;
 
     pub fn region_memory_buffer_alloc(buffer: *mut RegionMemoryBuffer, size: u64) -> *mut u8;
 
     pub fn region_memory_buffer_free(buffer: *mut RegionMemoryBuffer);
+}
+
+struct RegionAllocator {
+    region: RegionMemoryBuffer,
+}
+
+unsafe impl Send for RegionAllocator {}
+
+impl RegionAllocator {
+    fn new(size: usize) -> Self {
+        Self {
+            region: unsafe { create_region_memory_buffer(size as u64) },
+        }
+    }
+
+    unsafe fn alloc(&mut self, size: usize) -> Result<*mut u8, &'static str> {
+        let data =
+            region_memory_buffer_alloc(&mut self.region as *mut RegionMemoryBuffer, size as u64);
+
+        if data.is_null() {
+            Err("Out of memory")
+        }
+        else {
+            Ok(data)
+        }
+    }
+
+    unsafe fn clear(&mut self) -> Result<(), &'static str> {
+        region_memory_buffer_free(&mut self.region as *mut RegionMemoryBuffer);
+        Ok(())
+    }
+}
+
+pub mod render_state {
+    use crate::RegionAllocator;
+    use lazy_static::lazy_static;
+    use std::sync::Mutex;
+
+    lazy_static! {
+        static ref ALLOCATOR: Mutex<RegionAllocator> = Mutex::new(RegionAllocator::new(1024));
+    }
+
+    pub unsafe fn alloc(size: usize) -> Result<*mut u8, &'static str> {
+        let mut allocator = ALLOCATOR.lock().expect("failed to get allocator");
+        allocator.alloc(size)
+    }
+
+    pub unsafe fn clear() -> Result<(), &'static str> {
+        let mut allocator = ALLOCATOR.lock().expect("failed to get allocator");
+        allocator.clear()
+    }
+}
+
+pub mod render_commands {
+    use crate::RegionAllocator;
+    use lazy_static::lazy_static;
+    use std::sync::Mutex;
+
+    lazy_static! {
+        static ref ALLOCATOR: Mutex<RegionAllocator> = Mutex::new(RegionAllocator::new(1024));
+    }
+
+    pub unsafe fn alloc(size: usize) -> Result<*mut u8, &'static str> {
+        let mut allocator = ALLOCATOR.lock().expect("failed to get allocator");
+        allocator.alloc(size)
+    }
+
+    pub unsafe fn clear() -> Result<(), &'static str> {
+        let mut allocator = ALLOCATOR.lock().expect("failed to get allocator");
+        allocator.clear()
+    }
 }
 
 #[cfg(test)]
@@ -55,7 +129,8 @@ mod tests {
     fn test_region_memory_buffer_emplace_region() {
         unsafe {
             let mut region = create_region_memory_buffer(1024);
-            let sub_region = region_memory_buffer_emplace_region(&mut region as *mut RegionMemoryBuffer, 512);
+            let sub_region =
+                region_memory_buffer_emplace_region(&mut region as *mut RegionMemoryBuffer, 512);
 
             assert!(!sub_region.base.is_null());
             assert_eq!(512, region.offset);
