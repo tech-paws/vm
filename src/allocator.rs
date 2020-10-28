b075b6c3e290289148631c47e6821de0066b6726
Andrey Kabylin <andrey@kabylin.ru>
2020-10-28 14:42:31 +0700

    Move tests to examples

--- src/allocator.rs
+++ src/allocator.rs
@@ -3,11 +3,14 @@
 use crate::c_api::*;
 use std::mem;
 
-/// Simple region based allocator. Allocates continuous chunk of memory with a specific size.
+/// Simple region based allocator.
+///
+/// Allocates continuous chunk of memory with a specific size.
 /// Allocator maintain a pointer within that memory, whenever allocate an object,
 /// update the pointer by the object's size.
 pub struct RegionAllocator {
-    region: RegionMemoryBuffer,
+    /// The memory reserved for the allocator.
+    pub region: RegionMemoryBuffer,
 }
 
 unsafe impl Send for RegionAllocator {}
@@ -26,6 +29,15 @@ impl RegionAllocator {
     /// # Errors
     ///
     /// If the memory is run out, then this call will return an error.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use vm_memory_manager::allocator::*;
+    /// let mut allocator = RegionAllocator::new(1024);
+    /// let base = unsafe { allocator.alloc(512) };
+    /// assert!(base.is_ok());
+    /// ```
     pub unsafe fn alloc(&mut self, size: usize) -> Result<*mut u8, &'static str> {
         let data =
             region_memory_buffer_alloc(&mut self.region as *mut RegionMemoryBuffer, size as u64);
@@ -44,10 +56,27 @@ impl RegionAllocator {
         Ok(())
     }
 
-    /// Allocate a new region of memory with size = `size` of T and emplace a `value`
+    /// Allocate a new region of memory with size equals to size of `T` and emplace the `value`
     /// to the allocated memory.
     ///
     /// Returns a pointer to the struct located in the memory of the allocator.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use assert_approx_eq::assert_approx_eq;
+    /// # use std::mem;
+    /// # use vm_memory_manager::allocator::*;
+    /// # use vm_memory_manager::data::*;
+    /// #
+    /// let mut allocator = RegionAllocator::new(1024);
+    /// let vec = Vec2f::new(10., 20.);
+    /// let vec = unsafe { allocator.emplace_struct(vec).unwrap().as_ref().unwrap() };
+    ///
+    /// assert_eq!(mem::size_of::<Vec2f>(), allocator.region.offset);
+    /// assert_approx_eq!(10., vec.x);
+    /// assert_approx_eq!(20., vec.y);
+    /// ```
     pub unsafe fn emplace_struct<T>(&mut self, value: T) -> Result<*mut T, &'static str> {
         let value_ptr = &value as *const T;
         let data = region_memory_buffer_emplace(
@@ -61,27 +90,6 @@ impl RegionAllocator {
         }
         else {
             Ok(data as *mut T)
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use assert_approx_eq::assert_approx_eq;
-
-    use crate::allocator::*;
-    use crate::data::*;
-
-    #[test]
-    fn test_region_allocator_emplace_struct() {
-        unsafe {
-            let mut allocator = RegionAllocator::new(1024);
-            let vec = Vec2f::new(10., 20.);
-            let vec = allocator.emplace_struct(vec).unwrap().as_ref().unwrap();
-
-            assert_eq!(mem::size_of::<Vec2f>(), allocator.region.offset);
-            assert_approx_eq!(10., vec.x);
-            assert_approx_eq!(20., vec.y);
         }
     }
 }
--- src/c_api.rs
+++ src/c_api.rs
@@ -12,69 +12,101 @@ pub struct RegionMemoryBuffer {
 }
 
 extern "C" {
+    /// Allocate virtual memory.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use vm_memory_manager::c_api::*;
+    /// assert!(!unsafe { virtual_alloc(40) }.is_null());
+    /// ```
     pub fn virtual_alloc(size: i32) -> *mut u8;
 
+    /// Allocate a contiguous chunk of memory of a specific size.
+    ///
+    /// If out of memory, than `base` address will be `null`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use vm_memory_manager::c_api::*;
+    /// let region = unsafe { create_region_memory_buffer(1024) };
+    /// assert!(!region.base.is_null());
+    /// ```
     pub fn create_region_memory_buffer(size: u64) -> RegionMemoryBuffer;
 
+    /// Allocate a contiguous chunk of memory of a specific size inside another
+    /// [`RegionMemoryBuffer`].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use vm_memory_manager::c_api::*;
+    /// let mut region = unsafe { create_region_memory_buffer(1024) };
+    /// let sub_region =
+    ///     unsafe { region_memory_buffer_emplace_region(&mut region as *mut RegionMemoryBuffer, 512) };
+    ///
+    /// assert!(!sub_region.base.is_null());
+    /// assert_eq!(512, region.offset);
+    /// ```
     pub fn region_memory_buffer_emplace_region(
         buffer: *mut RegionMemoryBuffer,
         size: u64,
     ) -> RegionMemoryBuffer;
 
+    /// Allocate chunck of memory inside the `buffer`.
+    ///
+    /// The function will return the base address on success and `null` if an error occurs,
+    /// e.g. Out Of Memory.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use vm_memory_manager::c_api::*;
+    /// let mut region = unsafe { create_region_memory_buffer(1024) };
+    /// let base = unsafe { region_memory_buffer_alloc(&mut region as *mut RegionMemoryBuffer, 100) };
+    ///
+    /// assert!(!base.is_null());
+    /// assert_eq!(100, region.offset);
+    /// ```
     pub fn region_memory_buffer_alloc(buffer: *mut RegionMemoryBuffer, size: u64) -> *mut u8;
 
+    /// Emplace `data` to the `buffer`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use vm_memory_manager::c_api::*;
+    /// # use std::mem;
+    /// let mut region = unsafe { create_region_memory_buffer(1024) };
+    /// let value: i32 = 100;
+    /// let value_ptr = &value as *const i32;
+    /// let data = unsafe {
+    ///     region_memory_buffer_emplace(
+    ///         &mut region as *mut RegionMemoryBuffer,
+    ///         mem::size_of::<i32>() as u64,
+    ///         value_ptr as *const u8,
+    ///     )
+    /// };
+    /// let value = unsafe { data.as_ref().unwrap() };
+    /// assert_eq!(100, *value);
+    /// ```
     pub fn region_memory_buffer_emplace(
         buffer: *mut RegionMemoryBuffer,
         size: u64,
         data: *const u8,
     ) -> *mut u8;
 
+    /// Free `buffer` by move pointer to the `base` addres.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use vm_memory_manager::c_api::*;
+    /// let mut region = unsafe { create_region_memory_buffer(1024) };
+    /// unsafe { region_memory_buffer_alloc(&mut region as *mut RegionMemoryBuffer, 100) };
+    /// unsafe { region_memory_buffer_free(&mut region as *mut RegionMemoryBuffer) };
+    /// assert_eq!(0, region.offset);
+    /// ```
     pub fn region_memory_buffer_free(buffer: *mut RegionMemoryBuffer);
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::c_api::*;
-
-    #[test]
-    fn test_virtual_alloc() {
-        unsafe {
-            assert!(!virtual_alloc(40).is_null());
-        }
-    }
-
-    #[test]
-    fn test_create_region_memory_buffer() {
-        unsafe {
-            let region = create_region_memory_buffer(1024);
-            assert!(!region.base.is_null());
-        }
-    }
-
-    #[test]
-    fn test_alloc_in_region_buffer() {
-        unsafe {
-            let mut region = create_region_memory_buffer(1024);
-            let base = region_memory_buffer_alloc(&mut region as *mut RegionMemoryBuffer, 100);
-
-            assert!(!base.is_null());
-            assert_eq!(100, region.offset);
-
-            region_memory_buffer_free(&mut region as *mut RegionMemoryBuffer);
-
-            assert_eq!(0, region.offset);
-        }
-    }
-
-    #[test]
-    fn test_region_memory_buffer_emplace_region() {
-        unsafe {
-            let mut region = create_region_memory_buffer(1024);
-            let sub_region =
-                region_memory_buffer_emplace_region(&mut region as *mut RegionMemoryBuffer, 512);
-
-            assert!(!sub_region.base.is_null());
-            assert_eq!(512, region.offset);
-        }
-    }
 }
