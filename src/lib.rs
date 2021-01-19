c1e96d19db180ea2f8d8250e47ddc53e66edf1e7
Andrey Kabylin <andrey@kabylin.ru>
2021-01-19 05:53:42 +0000

    logging

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,10 @@ pub mod gapi;
 pub mod module;
 pub mod state;
 
+use std::{ffi::CStr, os::raw::c_char};
+
+use log;
+
 use crate::module::Module;
 use data::Commands;
 use state::VMState;
@@ -58,3 +62,33 @@ pub extern "C" fn tech_paws_vm_gapi_flush() {}
 
 #[no_mangle]
 pub extern "C" fn tech_paws_vm_flush() {}
+
+#[no_mangle]
+pub unsafe extern "C" fn tech_paws_vm_log_trace(message: *const c_char) {
+    let str = unsafe { CStr::from_ptr(message) }.to_str().unwrap();
+    log::trace!("{}", str);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn tech_paws_vm_log_error(message: *const c_char) {
+    let str = unsafe { CStr::from_ptr(message) }.to_str().unwrap();
+    log::error!("{}", str);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn tech_paws_vm_log_warn(message: *const c_char) {
+    let str = unsafe { CStr::from_ptr(message) }.to_str().unwrap();
+    log::warn!("{}", str);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn tech_paws_vm_log_debug(message: *const c_char) {
+    let str = unsafe { CStr::from_ptr(message) }.to_str().unwrap();
+    log::debug!("{}", str);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn tech_paws_vm_log_info(message: *const c_char) {
+    let str = unsafe { CStr::from_ptr(message) }.to_str().unwrap();
+    log::info!("{}", str);
+}
