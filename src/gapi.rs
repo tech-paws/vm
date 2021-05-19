a33b3381bf321499080f7f651441a5149d166ed4
Andrey Kabylin <andrey@kabylin.ru>
2021-05-19 15:56:30 +0100

    update mut

--- src/gapi.rs
+++ src/gapi.rs
@@ -6,15 +6,15 @@ use crate::{commands::gapi::draw_texts::TextData, commands_bus::CommandsBus};
 
 pub struct GApiContext<'a> {
     pub address: &'static str,
-    pub commands_bus: &'a CommandsBus,
+    pub commands_bus: &'a mut CommandsBus,
 }
 
-pub fn set_color_pipeline(context: &mut GApiContext, color: Vec4f) {}
+pub fn set_color_pipeline(context: &GApiContext, color: Vec4f) {}
 
-pub fn set_texture_pipeline(context: &mut GApiContext, id: u64) {}
+pub fn set_texture_pipeline(context: &GApiContext, id: u64) {}
 
-pub fn draw_centered_quads(context: &mut GApiContext, mvp_matrices: &[Mat4f]) {}
+pub fn draw_centered_quads(context: &GApiContext, mvp_matrices: &[Mat4f]) {}
 
-pub fn draw_quads(context: &mut GApiContext, mvp_matrices: &[Mat4f]) {}
+pub fn draw_quads(context: &GApiContext, mvp_matrices: &[Mat4f]) {}
 
-pub fn draw_texts(context: &mut GApiContext, texts: &[TextData]) {}
+pub fn draw_texts(context: &GApiContext, texts: &[TextData]) {}
