21a1d5677175016568c882bf8954d99eab89938d
Andrey Kabylin <andrey@kabylin.ru>
2021-05-26 05:30:12 +0100

    commands_bus documentation

--- src/commands_bus.rs
+++ src/commands_bus.rs
@@ -1,4 +1,6 @@
 //! Commands Bus
+//!
+//! Module implements abstraction for sending commands to a different modules.
 
 use parking_lot::lock_api::RawMutex;
 use vm_buffers::BytesWriter;
@@ -18,7 +20,7 @@ impl Default for CommandsBus {
 }
 
 impl CommandsBus {
-    /// Create a new commands bus.
+    /// Create a new `CommandsBus`.
     pub fn new() -> Self {
         CommandsBus {
             start_offset: 0,
@@ -26,14 +28,33 @@ impl CommandsBus {
         }
     }
 
-    /// Send command to address.
-    pub fn push_command<F>(
-        &self,
-        address: &'static str,
-        id: u64,
-        source: Source,
-        command_writer: F,
-    ) where
+    /// Send command to the module.
+    ///
+    /// Forms a command with the `id` and stores it to the `source` buffer
+    /// of the module at the `address`;
+    /// `commands_writer` is used to write the command payload.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use vm::{commands, commands_bus::CommandsBus, module};
+    ///
+    /// unsafe { vm::init() };
+    /// let commands_bus = CommandsBus::new();
+    /// commands_bus.push_command(
+    ///     module::CLIENT_ID,
+    ///     commands::gapi::SET_COLOR_PIPELINE,
+    ///     commands::Source::GAPI,
+    ///     |bytes_writer| {
+    ///         bytes_writer.write_f32(0.0); // r
+    ///         bytes_writer.write_f32(1.0); // g
+    ///         bytes_writer.write_f32(1.0); // b
+    ///         bytes_writer.write_f32(0.5); // a
+    ///     },
+    /// );
+    /// ```
+    pub fn push_command<F>(&self, address: &'static str, id: u64, source: Source, command_writer: F)
+    where
         F: FnOnce(&mut BytesWriter),
     {
         let state = unsafe { STATE.as_ref() }.unwrap();
@@ -73,6 +94,38 @@ impl CommandsBus {
         bytes_writer.write_u64_at(payload_size_offset, end_offset - start_offset);
     }
 
+    /// Start writing command.
+    ///
+    /// [`CommandsBus::begin_command`] and [`CommandsBus::end_command`] is an unsafe
+    /// alternative of [`CommandsBus::push_command`]. Can be useful for FFI.
+    /// Returns `bytes_writer` that used to write the command payload.
+    ///
+    /// # Safety
+    ///
+    /// * Should always end command.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use vm::{commands, commands_bus::CommandsBus, module};
+    /// use vm_buffers::BytesWriter;
+    ///
+    /// unsafe { vm::init() };
+    /// let mut commands_bus = CommandsBus::new();
+    /// let bytes_writer_raw = unsafe {
+    ///     commands_bus.begin_command(
+    ///         module::CLIENT_ID,
+    ///         commands::Source::GAPI,
+    ///         commands::gapi::SET_COLOR_PIPELINE,
+    ///     )
+    /// };
+    /// let mut bytes_writer = unsafe { BytesWriter::from_raw(*bytes_writer_raw) };
+    /// bytes_writer.write_f32(0.0); // r
+    /// bytes_writer.write_f32(1.0); // g
+    /// bytes_writer.write_f32(1.0); // b
+    /// bytes_writer.write_f32(0.5); // a
+    /// unsafe { commands_bus.end_command(module::CLIENT_ID, commands::Source::GAPI) };
+    /// ```
     pub unsafe fn begin_command(
         &mut self,
         address: &str,
@@ -115,6 +168,15 @@ impl CommandsBus {
         bytes_writer.raw()
     }
 
+    /// Finish writing command and send it to the module at the `address`.
+    ///
+    /// [`CommandsBus::begin_command`] and [`CommandsBus::end_command`] is an unsafe
+    /// alternative of [`CommandsBus::push_command`]. Can be useful for FFI.
+    ///
+    /// # Safety
+    ///
+    /// * Should always be used after [`CommandsBus::begin_command`] has been executed.
+    /// * `source` should be the same as it was in [`CommandsBus::begin_command`].
     pub unsafe fn end_command(&mut self, address: &str, source: Source) {
         let state = STATE.as_ref().unwrap();
         let module_state = &mut state.module_states.get(address).unwrap();
